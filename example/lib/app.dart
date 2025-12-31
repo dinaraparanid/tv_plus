264a14b326befc28db731b21e27c085632519e24
dinaraparanid <dinaraparanid@gmail.com>
2025-12-31 12:22:46 +0300

    test: cupertino tab bar

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'material/navigation/tab_bar_secondary_sample.dart';
+import 'cupertino/navigation/tab_bar_sample.dart';
 
 final class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const TvTabBarSecondarySample();
+    return const CupertinoTvTabBarSample();
   }
 }
