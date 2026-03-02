ecf590ccd1e2bea486a8bf613d47a2e1e0dafb5c
dinaraparanid <dinaraparanid@gmail.com>
2026-03-02 18:43:09 +0300

    multi-module fix

--- lib/src/one_ui/navigation/navigation.dart
+++
@@ -1,1 +0,0 @@
-export 'drawer/drawer.dart';
--- material/lib/src/navigation/drawer/drawer.dart
+++ material/lib/src/navigation/drawer/drawer.dart
@@ -2,7 +2,7 @@ library;
 
 import 'package:flutter/material.dart';
 
-import 'package:tv_plus_foundation/foundation.dart';
+import 'package:tv_plus_foundation/tv_plus_foundation.dart';
 
 part 'tv_navigation_drawer.dart';
 part 'tv_navigation_drawer_mode.dart';
